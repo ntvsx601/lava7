cac8f2e3d4024585fa01672aea4f6949f2e83eff
ntvsx601 <55246709+ntvsx601@users.noreply.github.com>
2024-05-03 17:38:31 +0200

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-e90d7193-8096-4630-9dd4-aade169cd365/"
 
 OUTPUT_DIR="logs"
 
-axelar_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+axelar_data=$(curl -A UA -x http://185.242.92.173:8257 --proxy-user khodqjxe:n6ewpb8br3f4 -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
