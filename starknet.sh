1b9ffff07382eb1a645164a2d315bf78b0eb846b
ntvsx601 <55246709+ntvsx601@users.noreply.github.com>
2025-09-28 00:17:53 +0300

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -6,7 +6,7 @@ OUTPUT_DIR="logs"
 
 UA=$(shuf -n 1 ua.txt)
 
-starknet_data=$(curl -A UA -x http://185.242.92.173:8257 --proxy-user khodqjxe:n6ewpb8br3f4 -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' "$RPC_URL")
+starknet_data=$(curl -A UA -x http://185.242.92.173:8257 --proxy-user khoxe:n6e8br3f4 -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
